3292a749227e73cd044f121c058a6818075b05fc
GLADYS <mungaiglado106@gmail.com>
2023-07-03 21:52:06 +0300

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
         <h2 className="text-4xl font-bold mb-4 text-orange-500">Welcome to Campus Events</h2>
         <p className="text-lg text-gray-200 mb-8">Explore and join exciting events happening on campus.</p>
         <a href="/events" className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded">
-          Get Started
+          Find Your Event
         </a>
       </div>
     </div>
