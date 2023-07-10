7c92d25586cd88ea1cadde243652beced4a69b59
GLADYS <mungaiglado106@gmail.com>
2023-07-11 00:38:30 +0300

    done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,29 @@
-import backgroundImage from '../assets/bg.jpg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Home() {
+  const [backgroundImage, setBackgroundImage] = useState('');
+
+  useEffect(() => {
+    const fetchRandomImage = async () => {
+      try {
+        const response = await axios.get('https://api.unsplash.com/photos/random', {
+          params: {
+            client_id: '_5Ql_GANS0Zk8MgfZeMkOL2mebq_gFvpA1NRPQDHcxE',
+          },
+        });
+
+        setBackgroundImage(response.data.urls.regular);
+      } catch (error) {
+        console.error('Error fetching random image:', error);
+      }
+    };
+
+    fetchRandomImage();
+  }, []);
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen ">
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <div className="bg-cover bg-center w-full h-full absolute top-15 left-0 z-0 bg-opacity-70" style={{ backgroundImage: `url(${backgroundImage})` }}></div>
       <div className="relative z-10 text-center">
         <h2 className="text-4xl font-bold mb-4 text-orange-500">Welcome to Campus Events</h2>
@@ -16,3 +37,4 @@ function Home() {
 }
 
 export default Home;
+
